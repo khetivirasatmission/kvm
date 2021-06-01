8d94de5761f5333f615063d8ae215a38a8f5b769
Shirish Goyal <shirish.goyal@gmail.com>
2021-06-01 10:35:17 +0530

    add gtag

--- gatsby-config.js
+++ gatsby-config.js
@@ -110,6 +110,17 @@ module.exports = {
     //     },
     //   },
     // },
+    {
+      resolve: `gatsby-plugin-gtag`,
+      options: {
+        // your google analytics tracking id
+        trackingId: `G-WG9YESFXQ0`,
+        // Puts tracking script in the head instead of the body
+        head: false,
+        // enable ip anonymization
+        anonymize: true,
+      },
+    },
   ],
 };
 
